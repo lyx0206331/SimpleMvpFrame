fcb2015427b32b8cd741523f4f004991a24341d2
lyx0206331 <rq880113@163.com>
2018-06-05 16:23:53 +0800

    parse to kotlin

--- app/src/main/java/com/adrian/simplemvpframe/MainActivity.java
+++ app/src/main/java/com/adrian/simplemvpframe/MainActivity.java
@@ -3,17 +3,29 @@ package com.adrian.simplemvpframe;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adrian.simplemvp.MvpPresenter;
 import com.adrian.simplemvp.MvpView;
 import com.adrian.simplemvp.base.BaseActivity;
+import com.adrian.simplemvpframe.views.DHealthyProgressView;
+import com.adrian.simplemvpframe.views.StepCounterProgressKt;
 import com.adrian.simplemvpframe.views.SuperCircleView;
 
 public class MainActivity extends BaseActivity implements MvpView {
 
     SuperCircleView mSuperCircleView;
     TextView textView;
+
+    private StepCounterProgressKt healthyProgressView;
+    private EditText et_input;
+    private Button bt_show;
+    private Button bt_reset;
+    private TextView tv_value;
 
     TextView text;
     MvpPresenter presenter;
@@ -23,6 +35,18 @@ public class MainActivity extends BaseActivity implements MvpView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initSuperCircle();
+
+        initStepCounter();
+
+        text = (TextView) findViewById(R.id.text);
+
+        //初始化Presenter
+        presenter = new MvpPresenter();
+        presenter.attachView(this);
+    }
+
+    private void initSuperCircle() {
         textView = (TextView) findViewById(R.id.tv);
         mSuperCircleView = (SuperCircleView) findViewById(R.id.superview);
         mSuperCircleView.setShowSelect(false);
@@ -38,12 +62,53 @@ public class MainActivity extends BaseActivity implements MvpView {
             }
         });
         valueAnimator.start();
+    }
 
-        text = (TextView)findViewById(R.id.text);
+    private void initStepCounter() {
+        et_input = (EditText) findViewById(R.id.et_input);
+        bt_show = (Button) findViewById(R.id.bt_show);
+        tv_value = (TextView) findViewById(R.id.tv_value);
+        bt_reset = findViewById(R.id.btn_reset);
 
-        //初始化Presenter
-        presenter = new MvpPresenter();
-        presenter.attachView(this);
+        healthyProgressView = (StepCounterProgressKt) findViewById(R.id.simple);
+
+        healthyProgressView.beginContinue(true);
+
+        healthyProgressView.setInterpolator(new AccelerateInterpolator());
+
+        healthyProgressView.setValue(17);
+
+        bt_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    float value = Float.valueOf(et_input.getText().toString().trim());
+                    if (value > 100) {
+                        throw new NumberFormatException();
+                    }
+                    healthyProgressView.setValue(value);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "请输入有效进度", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        bt_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                healthyProgressView.reset();
+            }
+        });
+
+        healthyProgressView.setOnValueChangeListener(new StepCounterProgressKt.OnValueChangeListener() {
+            @Override
+            public void onValueChange(float value) {
+                tv_value.setText((int) value + "");
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/adrian/simplemvpframe/views/StepCounterProgressKt.kt
+++ app/src/main/java/com/adrian/simplemvpframe/views/StepCounterProgressKt.kt
@@ -3,11 +3,9 @@ package com.adrian.simplemvpframe.views
 import android.animation.ValueAnimator
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.Color
-import android.graphics.DashPathEffect
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.Interpolator
 import android.widget.RelativeLayout
@@ -18,27 +16,54 @@ import com.adrian.simplemvpframe.R
  */
 class StepCounterProgressKt : RelativeLayout {
 
-    companion object {
-        public val TAG: String = StepCounterProgressKt::class.simpleName!!
-    }
+//    companion object {
+//        val TAG: String = StepCounterProgressKt::class.simpleName!!
+//    }
 
-    private val interpolator: Interpolator = AccelerateInterpolator()
-    private var basePaintColor: Int = Color.YELLOW
-    private var progressColor: Int = Color.WHITE
-    private var min: Float = 0f
-    private var begin: Float = 0f
-    private var max: Float = 100f
-    private lateinit var valueAnimator: ValueAnimator
-    private lateinit var valueChangeListener: OnValueChangeListener
-    private var value: Float = 0f
-    private var duration: Int = 1000
+    var interpolator: Interpolator? = null
+        set(value) {
+            field = value
+            if (valueAnimator != null) {
+                valueAnimator?.interpolator = interpolator
+            }
+        }
+    var basePaintColor: Int = Color.YELLOW
+    var progressColor: Int = Color.WHITE
+    var min: Float = 0f
+    var begin: Float = 0f
+    var max: Float = 100f
+    var valueAnimator: ValueAnimator? = null
+    var onValueChangeListener: OnValueChangeListener? = null
+    var value: Float = 0f
+        set(value) {
+            if (field >= max) {
+                log("超出最大值")
+                return
+            }
+            if (isContinue) {
+                field += value
+            } else {
+                begin = min
+                field = value
+            }
 
-    private var progressStrokeWidth: Int = 48
+            field = if (field > max) max else field
+            field = if (field < min) min else field
+
+            isComplete = false
+            invalidate()
+            if (value <= max || value >= min) {
+                animateValue()
+            }
+        }
+    var duration: Int = 1000
+
+    var progressStrokeWidth: Int = 48
     private var paddingT: Int = 0
     private var isComplete: Boolean = false
 
-    private var dashWidth = 5
-    private var dashSpace = 8
+    var dashWidth = 5
+    var dashSpace = 8
 
     private lateinit var progressPaint: Paint
     private var outPadding: Int = 0
@@ -46,7 +71,7 @@ class StepCounterProgressKt : RelativeLayout {
     private lateinit var baseCircle: RectF
     private lateinit var progressCircle: RectF
     private var baseStartAngle = 270f
-    private var baseFinishAngle = 359.8f
+    private var baseFinishAngle = 359.9f
     private var progressStartAngle = 270f
     private var plusAngle = 0f
     private var paddingR = 0
@@ -54,22 +79,24 @@ class StepCounterProgressKt : RelativeLayout {
     private var paddingB = 0
     private var isContinue = false
 
+    val log = { msg: String -> Log.e("StepCounterProgressKt", msg) }
+
     interface OnValueChangeListener {
         fun onValueChange(value: Float)
     }
 
     constructor(context: Context) : super(context)
 
-//    constructor(context: Context, attrs: AttributeSet) :super(context, attrs, 0) {}
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0) : super(context, attrs, defStyleAttr) {
         setWillNotDraw(false)
         val attributes: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.StepCounterProgress, defStyleAttr, 0)
 
-        paddingB = paddingBottom
-        paddingL = paddingLeft
-        paddingR = paddingRight
-        paddingT = paddingTop
+        paddingB = paddingBottom + 15
+        paddingL = paddingLeft + 15
+        paddingR = paddingRight + 15
+        paddingT = paddingTop + 15
 
         paddingT += progressStrokeWidth.shr(1)
         paddingB += progressStrokeWidth.shr(1)
@@ -85,6 +112,10 @@ class StepCounterProgressKt : RelativeLayout {
         max = attributes.getFloat(R.styleable.StepCounterProgress_max, max)
         duration = attributes.getInt(R.styleable.StepCounterProgress_duration, duration)
         progressStrokeWidth = attributes.getDimensionPixelOffset(R.styleable.StepCounterProgress_progress_stroke_width, progressStrokeWidth)
+
+        initBasePainter()
+        initProgressPainter()
+        initValueAnimator()
     }
 
     private fun initBasePainter() {
@@ -107,17 +138,89 @@ class StepCounterProgressKt : RelativeLayout {
 
     private fun initValueAnimator() {
         valueAnimator = ValueAnimator()
-        valueAnimator.interpolator = interpolator
-        valueAnimator.addUpdateListener(ValueAnimatorListenerImp())
+        valueAnimator!!.interpolator = interpolator
+        valueAnimator!!.addUpdateListener(ValueAnimatorListenerImp())
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        super.onLayout(changed, l, t, r, b)
+
+        log("onLayout")
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        log("onMeasure")
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        log("onSizeChanged")
+        initBaseRectF(h, w)
+        initProgressRectF(h, w)
+    }
+
+    private fun initBaseRectF(h: Int, w: Int) {
+        baseCircle = RectF()
+        baseCircle.set(paddingL.toFloat(), paddingT.toFloat(), (w - paddingR).toFloat(), (h - paddingB).toFloat())
+    }
+
+    private fun initProgressRectF(h: Int, w: Int) {
+        progressCircle = RectF()
+        progressCircle.set(paddingL.toFloat(), paddingT.toFloat(), (w - paddingR).toFloat(), (h - paddingB).toFloat())
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        log("onDraw,baseAngle:$baseFinishAngle -- plusAngle:$plusAngle")
+        canvas?.drawArc(baseCircle, baseStartAngle, baseFinishAngle, false, basePaint)
+        canvas?.drawArc(progressCircle, progressStartAngle, plusAngle, false, progressPaint)
+
+        if (isComplete) {
+            return
+        }
+        invalidate()
+    }
+
+    /**
+     * 执行属性动画
+     */
+    private fun animateValue() {
+        if (valueAnimator != null) {
+            valueAnimator?.setFloatValues(begin, value)
+            valueAnimator?.setDuration(duration.toLong())
+            valueAnimator?.start()
+        }
+    }
+
+    /**
+     * 开启连续模式，每次在原有的进度上增加进度
+     */
+    fun beginContinue(b: Boolean) {
+        isContinue = b
+    }
+
+    fun reset() = {
+        begin = min
+
+        invalidate()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        valueAnimator?.cancel()
     }
 
     inner class ValueAnimatorListenerImp : ValueAnimator.AnimatorUpdateListener {
         override fun onAnimationUpdate(animation: ValueAnimator?) {
             val value: Float = animation?.getAnimatedValue() as Float
 
-            this@StepCounterProgressKt.plusAngle = (baseFinishAngle * value) / max
-            if (valueChangeListener != null) {
-                valueChangeListener.onValueChange(value)
+            plusAngle = (baseFinishAngle * value) / max
+            if (onValueChangeListener != null) {
+                onValueChangeListener?.onValueChange(value)
             }
 
             begin = value
