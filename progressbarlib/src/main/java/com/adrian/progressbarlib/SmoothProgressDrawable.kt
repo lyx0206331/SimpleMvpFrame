4791122b63b9014d39353db556afead1fadaca78
lyx0206331 <lengyuxuan1534>
2018-07-30 01:33:51 +0800

    update progressbar

--- progressbarlib/src/main/java/com/adrian/progressbarlib/SmoothProgressDrawable.kt
+++ progressbarlib/src/main/java/com/adrian/progressbarlib/SmoothProgressDrawable.kt
@@ -3,10 +3,11 @@ package com.adrian.progressbarlib
 import android.graphics.*
 import android.graphics.drawable.Animatable
 import android.graphics.drawable.Drawable
+import android.os.SystemClock
 import android.support.annotation.UiThread
 import android.view.animation.Interpolator
-import com.sun.org.apache.bcel.internal.generic.FLOAD
 import org.jetbrains.annotations.NotNull
+import java.util.*
 
 /**
  * date:2018/7/27
@@ -95,6 +96,7 @@ class SmoothProgressDrawable : Drawable, Animatable {
             field = value
             invalidateSelf()
         }
+    var mIsNewTurn: Boolean = false
     var mIsMirrorMode: Boolean = false
         set(value) {
             if (field == value) return
@@ -116,7 +118,12 @@ class SmoothProgressDrawable : Drawable, Animatable {
             invalidateSelf()
         }
     var mBackgroundDrawable: Drawable? = null
-    var mUseGradient: Boolean = false
+        set(value) {
+            if (field == value) return
+            field = value
+            invalidateSelf()
+        }
+    var mUseGradients: Boolean = false
         set(value) {
             if (field == value) return
             field = value
@@ -134,7 +141,7 @@ class SmoothProgressDrawable : Drawable, Animatable {
     constructor(callbacks: Callbacks?, @NotNull interpolator: Interpolator, @NotNull mColors: IntArray?,
                 mSeparatorLength: Int, mSectionsCount: Int, mSpeed: Float, mProgressiveStartSpeed: Float,
                 mProgressiveStopSpeed: Float, mIsReversed: Boolean, mIsMirrorMode: Boolean,
-                mIsProgressiveStartActivated: Boolean, mStrokeWidth: Float, mBackgroundDrawable: Drawable, mUseGradient: Boolean) : super() {
+                mIsProgressiveStartActivated: Boolean, mStrokeWidth: Float, mBackgroundDrawable: Drawable, mUseGradients: Boolean) : super() {
         this.callbacks = callbacks
         this.interpolator = interpolator
         this.mColors = mColors
@@ -148,7 +155,7 @@ class SmoothProgressDrawable : Drawable, Animatable {
         this.mIsProgressiveStartActivated = mIsProgressiveStartActivated
         this.mStrokeWidth = mStrokeWidth
         this.mBackgroundDrawable = mBackgroundDrawable
-        this.mUseGradient = mUseGradient
+        this.mUseGradients = mUseGradients
 
         mMaxOffset = 1f / mSectionsCount
 
@@ -163,7 +170,7 @@ class SmoothProgressDrawable : Drawable, Animatable {
     }
 
     private fun refreshLinearGradientOptions() {
-        if (mUseGradient) {
+        if (mUseGradients) {
             mLinearGradientColors = IntArray(mSectionsCount + 2)
             mLinearGradientPositions = FloatArray(mSectionsCount + 2)
         } else {
@@ -220,24 +227,244 @@ class SmoothProgressDrawable : Drawable, Animatable {
         mPaint.shader = linearGradient
     }
 
+    @UiThread
+    private fun drawStrokes(canvas: Canvas) {
+        if (mIsReversed) {
+            canvas.translate(mBounds!!.width().toFloat(), 0f)
+            canvas.scale(-1f, 1f)
+        }
+
+        var prevValue: Float = 0f
+        var boundsWidth: Int = mBounds!!.width()
+        if (mIsMirrorMode) boundsWidth /= 2
+        var width: Int = boundsWidth + mSeparatorLength + mSectionsCount
+        var centerY = mBounds!!.centerY()
+        var xSectionWidth = 1f / mSectionsCount
+
+        var startX: Float
+        var endX: Float
+        var firstX: Float = 0f
+        var lastX: Float = 0f
+        var prev: Float
+        var end: Float
+        var spaceLength: Float
+        var xOffset: Float
+        var ratioSectionWidth: Float
+        var sectionWidth: Float
+        var drawLength: Float
+        var currentIndexColor = mColorsIndex
+
+        if (mStartSection == mCurrentSections && mCurrentSections == mSectionsCount) firstX = canvas.width.toFloat()
+
+        for (i in 0..mCurrentSections) {
+            xOffset = xSectionWidth * i + mCurrentOffset
+            prev = Math.max(0f, xOffset - xSectionWidth)
+            ratioSectionWidth = Math.abs(interpolator!!.getInterpolation(prev) - interpolator!!.getInterpolation(Math.min(xOffset, 1f)))
+            sectionWidth = width * ratioSectionWidth
+
+            spaceLength = if (sectionWidth + prev < width) Math.min(sectionWidth, mSeparatorLength.toFloat()) else 0f
+
+            drawLength = if (sectionWidth > spaceLength) sectionWidth - spaceLength else 0f
+            end = prevValue + drawLength
+            if (end > prevValue && i >= mStartSection) {
+                val xFinishingOffset: Float = interpolator!!.getInterpolation(Math.min(mFinishingOffset, 1f))
+                startX = Math.max(xFinishingOffset * width, Math.min(boundsWidth.toFloat(), prevValue))
+                endX = Math.min(boundsWidth.toFloat(), end)
+                drawLine(canvas, boundsWidth, startX, centerY.toFloat(), endX, centerY.toFloat(), currentIndexColor)
+                if (i == mStartSection) {   //first loop
+                    firstX = startX - mSeparatorLength
+                }
+            }
+            if (i == mCurrentSections) {
+                lastX = prevValue + sectionWidth    //keep the separator effect
+            }
+
+            prevValue = end + spaceLength
+            currentIndexColor = incrementColor(currentIndexColor)
+        }
+
+        drawBackgroundIfNeed(canvas, firstX, lastX)
+    }
+
+    @UiThread
+    private fun drawLine(canvas: Canvas, canvasWidth: Int, startX: Float, startY: Float, stopX: Float, stopY: Float, currentIndexColor: Int) {
+        mPaint.color = mColors!![currentIndexColor]
+
+        if (mIsMirrorMode) {
+            canvas.drawLine(startX, startY, stopX, stopY, mPaint)
+        } else {
+            if (mIsReversed) {
+                canvas.drawLine(canvasWidth + startX, startY, canvasWidth + stopX, stopY, mPaint)
+                canvas.drawLine(canvasWidth - startX, startY, canvasWidth - stopX, stopY, mPaint)
+            } else {
+                canvas.drawLine(startX, stopY, stopX, stopY, mPaint)
+                canvas.drawLine(canvasWidth * 2 - startX, startY, canvasWidth * 2 - stopX, stopY, mPaint)
+            }
+        }
+    }
+
+    @UiThread
+    private fun incrementColor(colorIndex: Int): Int {
+        val i = colorIndex + 1
+        return if (i >= mColors!!.size) 0 else i
+    }
+
+    @UiThread
+    private fun decrementColor(colorIndex: Int): Int {
+        val i = colorIndex - 1
+        return if (i < 0) mColors!!.lastIndex else i
+    }
+
+    /**
+     * start the animation with the first color.
+     * Calls progressiveStart(0)
+     */
+    @UiThread
+    private fun progressiveStart() {
+        progressiveStart(0)
+    }
+
+    /**
+     * start the animation from a given color.
+     */
+    @UiThread
+    private fun progressiveStart(index: Int) {
+        resetProgressiveStart(index)
+        start()
+    }
+
+    @UiThread
+    private fun resetProgressiveStart(index: Int) {
+        checkColorIndex(index)
+
+        mCurrentOffset = 0f
+        mIsFinishing = false
+        mFinishingOffset = 0f
+        mStartSection = 0
+        mCurrentSections = 0
+        mColorsIndex = index
+
+    }
+
+    private fun checkColorIndex(index: Int) {
+        if (index < 0 || index >= mColors!!.size) {
+            throw IllegalArgumentException(String.format(Locale.US, "Index %d not valid", index))
+        }
+    }
+
+    /**
+     * finish the animation by animating the remaining sections.
+     */
+    @UiThread
+    private fun progressiveStop() {
+        mIsFinishing = true
+        mStartSection = 0
+    }
+
+    @UiThread
+    private fun drawBackgroundIfNeed(canvas: Canvas, firstX: Float, lastX: Float) {
+        if (mBackgroundDrawable == null) return
+
+        fBackgroundRect.top = ((canvas.height - mStrokeWidth) / 2).toInt()
+        fBackgroundRect.bottom = ((canvas.height + mStrokeWidth) / 2).toInt()
+        fBackgroundRect.left = 0
+        fBackgroundRect.right = if (mIsMirrorMode) canvas.width / 2 else canvas.width
+        mBackgroundDrawable!!.bounds = fBackgroundRect
+
+        //draw the background if the animation is over
+        if (!mIsRunning) {
+            if (mIsMirrorMode) {
+                canvas.save()
+                canvas.translate(canvas.width / 2f, 0f)
+                drawBackground(canvas, 0f, fBackgroundRect.width().toFloat())
+                canvas.scale(-1f, 1f)
+                drawBackground(canvas, 0f, fBackgroundRect.width().toFloat())
+                canvas.restore()
+            } else {
+                drawBackground(canvas, 0f, fBackgroundRect.width().toFloat())
+            }
+            return
+        }
+
+        if (!mIsFinishing && !isStarting()) return
+
+        var tmpfirstX = firstX
+        var tmpLastX = lastX
+        if (tmpfirstX > tmpLastX) {
+            val temp: Float = tmpfirstX
+            tmpfirstX = tmpLastX
+            tmpLastX = temp
+        }
+
+        if (tmpfirstX > 0) {
+            if (mIsMirrorMode) {
+                canvas.save()
+                canvas.translate(canvas.width / 2f, 0f)
+                if (mIsReversed) {
+                    drawBackground(canvas, 0f, tmpfirstX)
+                    canvas.scale(-1f, 1f)
+                    drawBackground(canvas, 0f, tmpfirstX)
+                } else {
+                    drawBackground(canvas, canvas.width / 2 - tmpfirstX, canvas.width / 2f)
+                    canvas.scale(-1f, 1f)
+                    drawBackground(canvas, canvas.width / 2 - tmpfirstX, canvas.width / 2f)
+                }
+                canvas.restore()
+            } else {
+                drawBackground(canvas, 0f, tmpfirstX)
+            }
+        }
+
+        if (tmpLastX <= canvas.width) {
+            if (mIsMirrorMode) {
+                canvas.save()
+                canvas.translate(canvas.width / 2f, 0f)
+                if (mIsReversed) {
+                    drawBackground(canvas, tmpLastX, canvas.width / 2f)
+                    canvas.scale(-1f, 1f)
+                    drawBackground(canvas, tmpLastX, canvas.width / 2f)
+                } else {
+                    drawBackground(canvas, 0f, canvas.width / 2 - tmpLastX)
+                    canvas.scale(-1f, 1f)
+                    drawBackground(canvas, 0f, canvas.width / 2 - tmpLastX)
+                }
+                canvas.restore()
+            } else {
+                drawBackground(canvas, tmpLastX, canvas.width.toFloat())
+            }
+        }
+    }
+
+    @UiThread
+    private fun drawBackground(canvas: Canvas, fromX: Float, toX: Float) {
+        val count = canvas.save()
+        canvas.clipRect(fromX, (canvas.height - mStrokeWidth) / 2, toX, (canvas.height + mStrokeWidth) / 2)
+        mBackgroundDrawable!!.draw(canvas)
+        canvas.restoreToCount(count)
+    }
+
+    private fun isStarting(): Boolean {
+        return mCurrentSections < mSectionsCount
+    }
+
     override fun draw(canvas: Canvas?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun setAlpha(alpha: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        mPaint.alpha = alpha
     }
 
     override fun getOpacity(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return PixelFormat.TRANSPARENT
     }
 
     override fun setColorFilter(colorFilter: ColorFilter?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        mPaint.colorFilter = colorFilter
     }
 
     override fun isRunning(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return mIsRunning
     }
 
     override fun start() {
@@ -246,5 +473,37 @@ class SmoothProgressDrawable : Drawable, Animatable {
 
     override fun stop() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun scheduleSelf(what: Runnable?, `when`: Long) {
+        mIsRunning = true
+        super.scheduleSelf(what, `when`)
+    }
+
+    private val mUpdater: Runnable = Runnable {
+        run {
+            if (mIsFinishing) {
+                mFinishingOffset += OFFSET_PER_FRAME * mProgressiveStopSpeed
+                mCurrentOffset += OFFSET_PER_FRAME * mProgressiveStopSpeed
+                if (mFinishingOffset >= 1f) {
+                    stop()
+                }
+            } else if (isStarting()) {
+                mCurrentOffset += OFFSET_PER_FRAME * mProgressiveStartSpeed
+            } else {
+                mCurrentOffset += OFFSET_PER_FRAME * mSpeed
+            }
+
+            if (mCurrentOffset >= mMaxOffset) {
+                mIsNewTurn = true
+                mCurrentOffset -= mMaxOffset
+            }
+
+            if (mIsRunning) {
+                scheduleSelf(mUpdater, SystemClock.uptimeMillis() + FRAME_DURATION)
+            }
+
+            invalidateSelf()
+        }
     }
 }
